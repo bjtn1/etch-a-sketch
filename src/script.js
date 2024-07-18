759c75ceece1f7ee1d7b732b1e2301d46ce1709f
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-18 13:19:54 -0400

    Add event listener to all divs within the square container

--- src/script.js
+++ src/script.js
@@ -1,13 +1,28 @@
-function create_16_by_16_grid_of_divs() {
+function create_x_by_y_grid(x, y) {
   const div_container = document.querySelector("#container");
 
-  for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
+  for (let row = 0; row < x; row++) {
+    for (let col = 0; col < y; col++) {
       const div = document.createElement("div");
-      div.className += "square"
+      div.setAttribute("id", "square");
+      div.setAttribute("class", "square");
+
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#000000";
+      });
+
+      div.addEventListener("mouseout", () => {
+        div.style.backgroundColor = "#f1f5f9";
+      })
+
       div_container.appendChild(div);
     }
   }
 }
 
-create_16_by_16_grid_of_divs();
+
+function add_hover_to_x_by_y_grid() {
+
+}
+
+create_x_by_y_grid(16, 16);

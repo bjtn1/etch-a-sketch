1c12fc88439c102cfd9303810e98e75f9d920c91
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-19 00:12:15 -0400

    Calculate widht and height of sqaures in grin inside create_grid. Minor refactoring of funciton

--- src/script.js
+++ src/script.js
@@ -1,19 +1,28 @@
-function create_x_by_y_grid(x, y) {
-  const div_container = document.querySelector("#container");
+const div_container = document.querySelector("#container");
 
-  for (let row = 0; row < x; row++) {
-    for (let col = 0; col < y; col++) {
+
+function create_grid(size) {
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
       const div = document.createElement("div");
+
       div.setAttribute("id", "square");
-      div.setAttribute("class", "square");
+      div.classList.add("square");
+
+      let width   = size * 2;
+      let height  = size * 2;
+
+      div.style.width   = `${width}px`;
+      div.style.height  = `${height}px`;
+
 
       div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "#000000";
       });
 
-      div.addEventListener("mouseout", () => {
-        div.style.backgroundColor = "#f1f5f9";
-      })
+      // div.addEventListener("mouseout", () => {
+      //   div.style.backgroundColor = "#f1f5f9";
+      // })
 
       div_container.appendChild(div);
     }
@@ -25,4 +34,4 @@ function add_hover_to_x_by_y_grid() {
 
 }
 
-create_x_by_y_grid(16, 16);
+create_grid(16);

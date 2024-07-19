a5bc827bcf146dfc4b5ab920a7200ea1601f5915
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-19 00:20:56 -0400

    Move css from .css file to .js file

--- src/script.js
+++ src/script.js
@@ -2,6 +2,18 @@ const div_container = document.querySelector("#container");
 
 
 function create_grid(size) {
+  let container_width       = size * 32;
+  let container_height      = size * 32;
+  let container_bg_color    = "#f1f5f9";
+  let container_box_shadow  = "0 25px 50px -12px rgb(0 0 0 / 0.25)";
+
+  document.getElementById("container").style.width            = `${container_width}px`;
+  document.getElementById("container").style.height           = `${container_height}px`;
+  document.getElementById("container").style.backgroundColor  = `${container_bg_color}`;
+  document.getElementById("container").style.boxShadow        = `${container_box_shadow}`;
+  document.getElementById("container").style.display          = "flex";
+  document.getElementById("container").style.flexWrap         = "wrap";
+
   for (let row = 0; row < size; row++) {
     for (let col = 0; col < size; col++) {
       const div = document.createElement("div");
@@ -15,7 +27,6 @@ function create_grid(size) {
       div.style.width   = `${width}px`;
       div.style.height  = `${height}px`;
 
-
       div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "#000000";
       });
@@ -27,11 +38,6 @@ function create_grid(size) {
       div_container.appendChild(div);
     }
   }
-}
-
-
-function add_hover_to_x_by_y_grid() {
-
 }
 
 create_grid(16);

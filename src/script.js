d9a5e4436ed7f2f11ec859dbe8534d027f2e83fb
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-19 00:23:25 -0400

    Increase readability

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const div_container = document.querySelector("#container");
+const div_container = document.getElementById("container");
 
 
 function create_grid(size) {
@@ -7,12 +7,12 @@ function create_grid(size) {
   let container_bg_color    = "#f1f5f9";
   let container_box_shadow  = "0 25px 50px -12px rgb(0 0 0 / 0.25)";
 
-  document.getElementById("container").style.width            = `${container_width}px`;
-  document.getElementById("container").style.height           = `${container_height}px`;
-  document.getElementById("container").style.backgroundColor  = `${container_bg_color}`;
-  document.getElementById("container").style.boxShadow        = `${container_box_shadow}`;
-  document.getElementById("container").style.display          = "flex";
-  document.getElementById("container").style.flexWrap         = "wrap";
+  div_container.style.width            = `${container_width}px`;
+  div_container.style.height           = `${container_height}px`;
+  div_container.style.backgroundColor  = `${container_bg_color}`;
+  div_container.style.boxShadow        = `${container_box_shadow}`;
+  div_container.style.display          = "flex";
+  div_container.style.flexWrap         = "wrap";
 
   for (let row = 0; row < size; row++) {
     for (let col = 0; col < size; col++) {
